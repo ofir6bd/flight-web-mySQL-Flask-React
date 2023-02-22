b67a23752b75b9143f0c4af976cd2c05bb86f928
Ofir6bd <ofir6bd@gmail.com>
2023-02-22 13:34:47 +0200

    finished DB

--- tables_schema.py
+++ tables_schema.py
@@ -12,7 +12,6 @@ class UserRoles(db.Model):
     __tablename__ = 'user_roles'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     role_name = db.Column(db.String(50), nullable=False, unique=True)
-    users_table = db.relationship('Users', backref='UserRoles')
     
     def __repr__(self): # for print option
         return f'<Table: "UserRoles", role_name:"{self.role_name}">'
@@ -23,21 +22,20 @@ class Users(db.Model):
     password = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(50), nullable=False, unique=True)
     user_role = db.Column(db.Integer, db.ForeignKey('user_roles.id'))
-    administrators_table = db.relationship('Administrators', backref='Users')
-    customers_table = db.relationship('Customers', backref='Users')
-    airline_companies_table = db.relationship('AirlineCompanies', backref='Users')
+    user_role_ = db.relationship('UserRoles')
 
     def __repr__(self):
-        return f'<Table: "Users", username:"{self.username}, password:"{self.password}, email:"{self.email}, user_role:"{self.user_role}">'
+        return f'<Table: "Users", id:"{self.id}", username:"{self.username}, password:"{self.password}, email:"{self.email}, user_role:"{self.user_role}">'
 
 class Administrators(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     first_name = db.Column(db.String(50), nullable=False)
     last_name = db.Column(db.String(50), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    user = db.relationship('Users')
 
     def __repr__(self):
-        return f'<Table: "Administrators", first_name:"{self.first_name}, last_name:"{self.last_name}, user_id:"{self.user_id}">'
+        return f'<Table: "Administrators",id:"{self.id}", first_name:"{self.first_name}, last_name:"{self.last_name}, user_id:"{self.user_id}">'
 
 class Customers(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
@@ -47,18 +45,17 @@ class Customers(db.Model):
     phone_no = db.Column(db.String(50), nullable=False, unique=True)
     credit_card_no = db.Column(db.String(50), nullable=False, unique=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    tickets_table = db.relationship('Tickets', backref='Customers')
+    user = db.relationship('Users')
 
     def __repr__(self):
-        return f'<Table: "Customers", first_name:"{self.first_name}, last_name:"{self.last_name}, address:"{self.address},phone_no:"{self.phone_no}, credit_card_no:"{self.credit_card_no}, user_id:"{self.user_id} ">'
+        return f'<Table: "Customers",id:"{self.id}", first_name:"{self.first_name}, last_name:"{self.last_name}, address:"{self.address},phone_no:"{self.phone_no}, credit_card_no:"{self.credit_card_no}, user_id:"{self.user_id} ">'
 
 class Countries(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(50), nullable=False, unique=True)
-    flights_table_origin_country = db.relationship('Flights', backref='Countries')
-
+    
     def __repr__(self):
-        return f'<Table: "Countries", name:"{self.name}">'
+        return f'<Table: "Countries",id:"{self.id}", name:"{self.name}">'
 
 class AirlineCompanies(db.Model):
     __tablename__ = 'airline_companies'
@@ -66,32 +63,34 @@ class AirlineCompanies(db.Model):
     name = db.Column(db.String(50), nullable=False, unique=True)
     country_id = db.Column(db.Integer, db.ForeignKey('countries.id'))
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    flights_table_origin_country = db.relationship('Flights', backref='AirlineCompanies')
+    user = db.relationship('Users')
     
     def __repr__(self):
-        return f'<Table: "AirlineCompanies", name:"{self.name}, country_id:"{self.country_id}, user_id:"{self.user_id}">'
+        return f'<Table: "AirlineCompanies",id:"{self.id}", name:"{self.name}, country_id:"{self.country_id}, user_id:"{self.user_id}">'
 
 class Flights(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     airline_company_id = db.Column(db.Integer, db.ForeignKey('airline_companies.id'))
+    airline_company = db.relationship('AirlineCompanies')
     origin_country_id = db.Column(db.Integer, db.ForeignKey('countries.id'))
-    destination_country_id = db.Column(db.String(50), nullable=False)  #TODO this should be a forien key column
+    origin_country = db.relationship('Countries', foreign_keys=[origin_country_id])
+    destination_country_id = db.Column(db.Integer, db.ForeignKey('countries.id'))
+    destination_country = db.relationship('Countries', foreign_keys=[destination_country_id])
     departure_time = db.Column(db.DateTime, nullable=False)
     landing_time = db.Column(db.DateTime, nullable=False)
     remaining_tickets = db.Column(db.Integer, default=0)
-    tickets_table = db.relationship('Tickets', backref='Flights')
-    #TODO destination_country_id = db.Column(db.Integer, db.ForeignKey('countries.id'))
-
+    
     def __repr__(self):
-        return f'<Table: "Flights", airline_company_id:"{self.airline_company_id}, origin_country_id:"{self.origin_country_id}",departure_time:"{self.departure_time}",landing_time:"{self.landing_time}",remaining_tickets:"{self.remaining_tickets}">'
+        return f'<Table: "Flights",id:"{self.id}", airline_company_id:"{self.airline_company_id}, origin_country_id:"{self.origin_country_id}",destination_country_id:"{self.destination_country_id}",departure_time:"{self.departure_time}",landing_time:"{self.landing_time}",remaining_tickets:"{self.remaining_tickets}">'
 
 class Tickets(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     flight_id = db.Column(db.Integer, db.ForeignKey('flights.id'))
+    flight = db.relationship('Flights')
     customer_id = db.Column(db.Integer, db.ForeignKey('customers.id'))
+    customer = db.relationship('Customers')
     
     def __repr__(self):
-        return f'<Table: "Tickets", flight_id:"{self.flight_id}",customer_id:"{self.customer_id}">'
-
+        return f'<Table: "Tickets",id:"{self.id}", flight_id:"{self.flight_id}",customer_id:"{self.customer_id}">'
 
 
