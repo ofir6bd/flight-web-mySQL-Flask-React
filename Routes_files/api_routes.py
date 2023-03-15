281a552c637c780cf75ed2541f18cc7c4f6ff762
Ofir6bd <ofir6bd@gmail.com>
2023-03-15 16:18:49 +0200

    finish API wrapper

--- Routes_files/api_routes.py
+++ Routes_files/api_routes.py
@@ -25,46 +25,40 @@ from Facades.AirlineFacade import AirlineFacade
 from Forms_templates.general_forms import search_flights_form
 from flask import jsonify
 from flask_bcrypt import Bcrypt,generate_password_hash, check_password_hash
+from per_req_Wrappers import require_api_auth
 
-# @login_required
-# @require_customer_role
-# def customer_home(customer_details):
-#     fac_obj = CustomerFacade(user_id=customer_details)
-#     user = fac_obj.get_customer_by_user_id()
-#     full_name = f'{user.last_name}, {user.first_name}' 
-#     return render_template("customer/customer_home.html",
-#         customer_details = customer_details,
-#         text=full_name,
-#         title=customer_details,
-#         )
-# from app import create_app
-# app = create_app()
-# from models import UserRoles,Users,Administrators,Customers, Countries,AirlineCompanies,Flights,Tickets
 
-def api_login(email,password):
-    fac_obj = CustomerFacade(email=email)
-    user = fac_obj.get_user_by_email()
-    if check_password_hash(user.password, password):
-        login_user(user)
-        session['user_id'] = user.id
+# def delete_sessions():
+#     session.pop('user_id', None)
+#     session.pop('user_role', None)
+#     session.pop('admin_id', None)
+#     session.pop('airline_id', None)
+#     session.pop('customer_id', None)
 
-        fac_obj = AnonymousFacade(user_id=int(user.id))
-        admin = fac_obj.get_admin_by_user_id()
-        airline = fac_obj.get_airline_by_user_id()
-        customer = fac_obj.get_customer_by_user_id()
+# def api_login(email,password):
+#     fac_obj = CustomerFacade(email=email)
+#     user = fac_obj.get_user_by_email()
+#     if check_password_hash(user.password, password):
+#         login_user(user)
+#         session['user_id'] = user.id
 
-        if admin:
-            session['user_role'] = 'admin'
-            session['admin_id'] = admin.id
-        elif airline:
-            session['user_role'] = 'airline'
-            session['airline_id'] = airline.id
-        elif customer:
-            session['user_role'] = 'customer'
-            session['customer_id'] = customer.id
-        else:
-            session['user_role'] = 'general_user'
-    return
+#         fac_obj = AnonymousFacade(user_id=int(user.id))
+#         admin = fac_obj.get_admin_by_user_id()
+#         airline = fac_obj.get_airline_by_user_id()
+#         customer = fac_obj.get_customer_by_user_id()
+
+#         if admin:
+#             session['user_role'] = 'admin'
+#             session['admin_id'] = admin.id
+#         elif airline:
+#             session['user_role'] = 'airline'
+#             session['airline_id'] = airline.id
+#         elif customer:
+#             session['user_role'] = 'customer'
+#             session['customer_id'] = customer.id
+#         else:
+#             session['user_role'] = 'general_user'
+#     return
 
 def api_get_all_countries():
     dal_obj = AnonymousFacade(api=True)
@@ -74,31 +68,33 @@ def api_get_all_flights():
     dal_obj = AnonymousFacade(api=True)
     return dal_obj.get_all_flights()
 
+@require_api_auth
 def api_get_my_tickets():
-    email = request.args.get('email')
-    password = request.args.get('password')
-    api_login(email,password)
+    print(current_user)
     if not current_user.is_authenticated:
         return jsonify({ 'error': 'email or password are incorrect'})
     else:
         if session['user_role'] == 'customer':
             fac_obj = CustomerFacade(api=True,customer_id=session['customer_id'])
             return fac_obj.get_my_ticket()
-        
+
+@require_api_auth
 def api_delete_my_ticket(ticket_id):
-    email = request.args.get('email')
-    password = request.args.get('password')
-    api_login(email,password)
     if not current_user.is_authenticated:
         return jsonify({ 'error': 'Email or password are incorrect'})
     else:
         if session['user_role'] == 'customer':
             fac_obj = CustomerFacade(api=True,id=ticket_id)
             ticket = fac_obj.get_ticket_by_id()
-            if ticket.customer_id == session['customer_id']:
-                res = fac_obj.remove_ticket()
-                if res:
-                   return jsonify({ 'result': 'Ticket removed'}) 
+            if ticket:
+                if ticket.customer_id == session['customer_id']:
+                    res = fac_obj.remove_ticket()
+                    if res:
+                        return jsonify({ 'result': 'Ticket removed'}) 
+                else:
+                    return jsonify({ 'error': 'Cannot delete flight that is not yours'}) 
+            else:
+                return jsonify({ 'Error': 'Ticket not found'}) 
     
         
 
--- per_req_Wrappers.py
+++ per_req_Wrappers.py
@@ -1,7 +1,9 @@
 
 from functools import wraps
 from flask import Flask, redirect, url_for,request, render_template, session, flash
-
+from Facades.AnonymousFacade import AnonymousFacade
+from flask_bcrypt import Bcrypt,generate_password_hash, check_password_hash
+from flask_login import login_user,logout_user
 # admin wrapper 
 def require_admin_role(func):
     @wraps(func)
@@ -31,4 +33,50 @@ def require_customer_role(func):
             flash('You are not authorized to open the requested page, welcome back to home page!')
             return redirect(url_for('index'))  
         return func(*args, **kwargs)
+    return wrapper
+
+
+
+
+
+def api_login(email,password):
+    fac_obj = AnonymousFacade(email=email)
+    user = fac_obj.get_user_by_email()
+    if check_password_hash(user.password, password):
+        login_user(user)
+        session['user_id'] = user.id
+
+        fac_obj = AnonymousFacade(user_id=int(user.id))
+        admin = fac_obj.get_admin_by_user_id()
+        airline = fac_obj.get_airline_by_user_id()
+        customer = fac_obj.get_customer_by_user_id()
+
+        if admin:
+            session['user_role'] = 'admin'
+            session['admin_id'] = admin.id
+        elif airline:
+            session['user_role'] = 'airline'
+            session['airline_id'] = airline.id
+        elif customer:
+            session['user_role'] = 'customer'
+            session['customer_id'] = customer.id
+        else:
+            session['user_role'] = 'general_user'
+    return
+
+# api wrapper 
+def require_api_auth(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        email = request.args.get('email')
+        password = request.args.get('password')
+        api_login(email,password)
+        res = func(*args, **kwargs) # calling the function
+        logout_user()
+        session.pop('user_id', None)
+        session.pop('user_role', None)
+        session.pop('admin_id', None)
+        session.pop('airline_id', None)
+        session.pop('customer_id', None)
+        return res
     return wrapper
