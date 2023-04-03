3ee9152dc907bf71f3b43d13a9e35a5b2c2bc975
Ofir6bd <ofir6bd@gmail.com>
2023-04-03 18:51:48 +0300

    stop point

---
+++ flight_react/src/apiHandler/apiHandlerCustomer.js
@@ -0,0 +1,13 @@
+export function apiAddTicket(email, password, flight_id) {
+  console.log("start api apiAddTicket");
+  const requestOptions = { method: "POST" };
+  var url =
+    "http://127.0.0.1:5000/API/customer/add_ticket?email=" +
+    email +
+    "&password=" +
+    password +
+    "&flight_id=" +
+    flight_id;
+  var response = fetch(url, requestOptions).then((response) => response.json());
+  return response;
+}
--- flight_react/src/components/Auth/Auth.js
+++ flight_react/src/components/Auth/Auth.js
@@ -1,34 +1,22 @@
 import { checkLogin, checkAdmin } from "../../apiHandler/ApiHandler";
-import React, { useState } from "react";
+import React, { Component, useState } from "react";
 
+export const handleUserID = (email, password, val) => {
+  checkAdmin(email, password, val)
+    .then((response) => console.log(response))
+    .catch((err) => console.log(err));
+};
 
-export function Auth(email, password) {
-
-}
-
-// export function Auth(email, password) {
-//   const [userID, setUserID] = useState("");
-
-//   const handleUserID = (val) => {
-//     setUserID(val);
-//     checkAdmin(email, password, val)
-//       .then((response) => console.log(response))
-//       .catch((err) => console.log(err));
-//   };
-
-//   const Authentication = (email, password) => {
-//     console.log("start Auth");
-//     checkLogin(email, password)
-//       .then((response) => {
-//         if (response.user_id) {
-//           handleUserID(response.user_id);
-//         }
-//       })
-//       .then(() => {
-//         console.log("data");
-//       })
-//       .catch((err) => console.log(err));
-//   };
-
-//   return null;
-// }
+export const Auth = (email, password) => {
+  console.log("start Auth");
+  checkLogin(email, password)
+    .then((response) => {
+      if (response.user_id) {
+        handleUserID(email, password, response.user_id);
+      }
+    })
+    .then(() => {
+      console.log("data");
+    })
+    .catch((err) => console.log(err));
+};
