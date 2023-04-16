bdd3f84dec4529ce042a809635d9ce5f6e7aeb79
Ofir6bd <ofir6bd@gmail.com>
2023-04-16 17:06:27 +0300

    verification

---
+++ flight_react/src/components/verification/verification.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+import { Button } from "@mui/material";
+import "./verification.css";
+import { apiAddTicket } from "../../apiHandler/apiHandlerCustomer";
+import { useNavigate } from "react-router";
+
+
+function Verification(flightId) {
+  let navigate = useNavigate();
+
+  const handleClick = () => {
+    apiAddTicket(
+      localStorage.getItem("globalVarEmail"),
+      localStorage.getItem("globalVarPassword"),
+      flightId
+    );
+    navigate("/");
+  };
+
+  return (
+    <div className="container">
+      <h2> Verification page</h2>
+      <h4> Are you sure you want to book this flight?</h4>
+      <Button variant="contained" className="Button" onClick={handleClick}>
+        Sure
+      </Button>
+    </div>
+  );
+}
+
+export default Verification;
