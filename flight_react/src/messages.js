bb07209c96d5aa883b554324f9832756ec61e1bf
Ben David <ofir.ben.david@intel.com>
2023-04-24 14:41:24 +0300

    fixing messages

--- flight_react/src/messages.js
+++ flight_react/src/messages.js
@@ -1,29 +1,40 @@
 import React from "react";
 
+function Message(props) {
+  return <div className="messageContainerError">{props.message}</div>;
+}
+
 function Messages(props) {
   const { message, messageType } = props;
 
-  console.log(message);
+  // console.log(message);
   localStorage.removeItem("globalVarMessage");
   localStorage.removeItem("globalVarMessageType");
   if (!message) {
     return null;
   }
-
-  // console.log(message.map((d) => d.toString()));
-
-  if (messageType == "success") {
+  console.log("message", message);
+  console.log("mess", message.includes("error"));
+  if (messageType === "success") {
     return <div className="messageContainerSuccess"> {message}</div>;
-  } else if (messageType == "error") {
+  } else if (message.includes("error")) {
+    try {
+      const errorArray = JSON.parse(message);
+
+      return (
+        <div>
+          {errorArray.map((error) => {
+            return <Message message={error.error} />;
+          })}
+        </div>
+      );
+    } catch (e) {
+      console.log("Error parsing message:", e);
+      return <div className="messageContainerError"> {message}</div>;
+    }
+  } else if (messageType === "error") {
     return <div className="messageContainerError"> {message}</div>;
-    // return (
-    //   <div className="messageContainerError">
-    //     {message.map((msg, index) => (
-    //       <p key={index}>{msg}</p>
-    //     ))}
-    //   </div>
-    // );
-  } else if (messageType == "warning") {
+  } else if (messageType === "warning") {
     return <div className="messageContainerWarning"> {message}</div>;
   }
 }
